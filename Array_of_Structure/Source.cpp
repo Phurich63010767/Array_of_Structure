e79e88b471b8572e63374d0ddd0e18d27c9f258b
Phurich63010767 <69179855+Phurich63010767@users.noreply.github.com>
2020-08-28 00:35:34 +0700

    Update Source.cpp

--- Array_of_Structure/Source.cpp
+++ Array_of_Structure/Source.cpp
@@ -1,43 +1,47 @@
-#include<iostream> 
-#include<math.h>
+#include<iostream>
+#include<cmath>
+
 using namespace std;
 
 struct Point {
     int x, y;
 };
 
+void printStruct(Point a[],int i)
+{
+    cout << a[i].x << " " << a[i].y << endl;
+};
+
 int main()
 {
-START:
+    START:
     int n;
     int t;
     cout << "Enter first number: ";
     cin >> n;
     cout << "Enter second number: ";
     cin >> t;
-    int a = 0;
+
     struct Point arr[50];
     if (n == t) {
-        cout << "Cannot enter same number." << endl;
-        system("pause");
-        system("CLS");
+        cout << "Cannot enter same number."<< endl;
         goto START;
     }
-    while (a <= t ) {
+    for(int a=0;a<t;++a) {
         if (n < t) {
-            arr[a].x = n;
+            arr[a].x = n ;
             arr[a].y = n + 1;
-            cout << arr[a].x << " " << arr[a].y << endl;
+            //cout << arr[a].x << " " << arr[a].y << endl;
+            printStruct(arr,a);
             n++;
         }
         if (t < n) {
             arr[a].x = t;
             arr[a].y = t + 1;
-            cout << arr[a].x << " " << arr[a].y << endl;
+            //cout << arr[a].x << " " << arr[a].y << endl;
+            printStruct(arr,a);
             t++;
         }
-        a++;
     }
-    system("pause");
     return 0;
 }
