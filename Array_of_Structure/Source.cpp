47049194918755e605d09c44fdea580ec273dbe5
Phurich63010767 <69179855+Phurich63010767@users.noreply.github.com>
2020-08-28 23:37:19 +0700

    Update Source.cpp

--- Array_of_Structure/Source.cpp
+++ Array_of_Structure/Source.cpp
@@ -7,42 +7,73 @@ struct Point {
     int x, y;
 };
 
-void printStruct(Point a[],int i)
+void printStruct(Point a[], int i)
 {
-    cout << a[i].x << " " << a[i].y << endl;
+    cout << a[i].x << " " << a[i].y << "   -> Line: " << i+1 << endl;
 };
 
 int main()
 {
-    START:
-    int n;
+START:
+    int n, sum = 0;
     int t;
-    cout << "Enter first number: ";
-    cin >> n;
-    cout << "Enter second number: ";
-    cin >> t;
-
-    struct Point arr[50];
-    if (n == t) {
-        cout << "Cannot enter same number."<< endl;
-        system("pause");
-        system("CLS");
-        goto START;
-    }
-    for(int a=0;a<t;++a) {
-        if (n < t) {
-            arr[a].x = n ;
-            arr[a].y = n + 1;
-            //cout << arr[a].x << " " << arr[a].y << endl;
-            printStruct(arr,a);
-            n++;
+    
+        cout << "Enter first number: ";
+        cin >> n;
+        cout << "Enter second number: ";
+        cin >> t;
+    
+        struct Point arr[50];
+        if (n == t) {
+            cout << "Cannot enter same number." << endl;
+            goto START;
         }
+    for (int z = 0; z < 3;z++) 
+    {
         if (t < n) {
-            arr[a].x = t;
-            arr[a].y = t + 1;
-            //cout << arr[a].x << " " << arr[a].y << endl;
-            printStruct(arr,a);
-            t++;
+            for (int a = 0;a < t;a++)
+            {
+                if (t < n) {
+                    arr[a].x = t ;
+                    arr[a].y = t + 1 ;
+
+                    printStruct(arr, a);
+                    t++;
+                    if (a % 2 == 0)
+                    {
+                        sum += arr[a].x + arr[a].y;
+                    }
+                    if (t == n)
+                    {
+                        t = arr[0].x;
+                    }
+                }
+
+            }
+            cout << "sum is " << sum << endl;
+            sum = 0;
+        }
+        if (n < t) {
+            for (int a = 0;a < n;a++)
+            {
+                if (n < t) {
+                    arr[a].x = n ;
+                    arr[a].y = n + 1 ;
+
+                    printStruct(arr, a);
+                    n++;
+                    if (a % 2 == 0)
+                    {
+                        sum += arr[a].x + arr[a].y;
+                    }
+                    if (n == t)
+                    {
+                        n = arr[0].x;
+                    }
+                }
+            }
+            cout << "sum is " << sum << endl;
+            sum = 0;
         }
     }
     return 0;
